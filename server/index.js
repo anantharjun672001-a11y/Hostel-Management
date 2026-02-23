619d133583c229079fecfe3eb6824197b3d4f8e5
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-23 22:18:39 +0530

    basic backend completed

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+    console.log("Mongo URL:", process.env.MONGO_URL);
+app.get("/", (req, res) => {
+  res.send("Hostel API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
