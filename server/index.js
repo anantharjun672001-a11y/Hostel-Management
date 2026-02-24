c09865095279fbdaf445ff7384c6524f9391c1d6
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-24 17:23:39 +0530

    auth completed in backend

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.use(express.json());
 app.use(cors());
---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["admin","staff","resident"],
+        default:"resident"
+    }
+    
+})
+
+const User = mongoose.model("User",userSchema)
+
+export default User;
