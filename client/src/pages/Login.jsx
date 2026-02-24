02cdf13e63fdf01d7cb8055a6251eb41024b11ee
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-24 17:34:37 +0530

    sample login page created

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+
+const Login = () => {
+    const [data,setData] = useState({ email:"",password:""})
+
+    const handleLogin = async () =>{
+        const res = await axios.post("http://localhost:5000/api/auth/login",data);
+        console.log(res);
+    }
+
+    return (
+        <div className='h-screen flex items-center justify-center bg-gray-100'>
+            <div className='p-6  shadow-lg'>
+                <input 
+                    type="email"
+                    placeholder='Email'
+                    className='border p-2 mb-4 w-full'
+                    value={data.email}
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                />
+                <input 
+                    type="password"
+                    placeholder='Password'
+                    className='border p-2 mb-4 w-full'
+                    value={data.password}
+                    onChange={(e) => setData({ ...data, password: e.target.value })}
+                />
+                <button className='bg-blue-500 text-white p-2 w-full' onClick={handleLogin}>Login</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
