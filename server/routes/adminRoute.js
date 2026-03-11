549e2f59419a7e1b773ad34d96a16d9a06e63b8e
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-11 22:32:47 +0530

    api name changed

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -4,7 +4,7 @@ import {createStaff, createUser} from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.post("/create-resident",createUser);
+router.post("/create-user",createUser);
 router.post("/create-staff",createStaff);
 
 export default router;
