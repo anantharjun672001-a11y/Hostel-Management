c7c675b31fd97fc3884f5fb32ce86d106259aa3e
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-12 08:23:56 +0530

    login altered

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -84,15 +84,7 @@ const Login = () => {
 
         </form>
 
-        <p className="text-center text-sm text-gray-500 mt-6">
-          Don't have an account?{" "}
-          <Link
-            to="/register"
-            className="text-blue-600 hover:underline font-medium"
-          >
-            Register
-          </Link>
-        </p>
+
 
       </div>
 
